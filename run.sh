07b01c28cf085e2a5d84c7258b1dcb78c3a8129b
Weidong <zhaoweidong@gouuse.cn>
2019-05-16 11:31:22 +0800

    update.

--- run.sh
+++ run.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-logpath=$1
+# logpath=$1
 
-if [ ! -n "$logpath" ]; then
-    echo "chmod log permision failed"
-else
-    gosu root chown -R zceo:zceo "$logpath"
-fi
+# if [ ! -n "$logpath" ]; then
+#     echo "chmod log permision failed"
+# else
+#     gosu root chown -R zceo:zceo "$logpath"
+# fi
+
+echo 'successful.'
